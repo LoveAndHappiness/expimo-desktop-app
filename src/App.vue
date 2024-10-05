d52c704246b0b4e09808e143a211e26fb794fa4d
Georg Geladaris <georg@geladaris.com>
2024-10-05 15:23:40 +0200

    feat: integrated Vue 3 into Electron application

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- src/App.vue -->
+<template>
+    <div id="app">
+      <h1>Expimo Desktop App</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style>
+  /* Add your styles here */
+  </style>
+  
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,28 @@
-const rules = require('./webpack.rules');
+// webpack.renderer.config.js
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+const rules = require('./webpack.rules');
+const { VueLoaderPlugin } = require('vue-loader');
+
+rules.push(
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader',
+  }
+);
 
 module.exports = {
-  // Put your normal webpack config below here
+  entry: './src/renderer.js',  // New line added
   module: {
     rules,
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
   },
 };
